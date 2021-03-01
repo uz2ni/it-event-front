315f8553f4ad52e9734991a5e34ef8a12e6deca9
soraji <wlthfk0211@gmail.com>
2021-03-01 16:48:10 +0900

    main edit

--- src/components/main.vue
+++ src/components/main.vue
@@ -2,21 +2,22 @@
   <div>
     <div class="main_txt">개발자를 위한 컨퍼런스 아카이빙 사이트
       <div class="searchBox">
-        <div><input type="text" placeholder="컨퍼런스명 검색"></div>
+        <div><input type="text" id="search" placeholder="컨퍼런스명 검색"></div>
         <div>
-          <select name="" id="">
+          <select name="onlineyn" id="onlineyn" v-model="onlineyn">
             <option value="00">전체</option>
             <option value="01">온라인</option>
             <option value="02">오프라인</option>
           </select>
         </div>
         <div>
-          <select name="" id="">
+          <select name="whom" id="whom" v-model="whom">
             <option value="00">개발자</option>
             <option value="01">기획자</option>
             <option value="02">디자이너</option>
           </select>
         </div>
+        <div><button type="button" class="searchBtn"></button>FIND</div>
       </div>
     </div>
     <div class="list"></div>
@@ -31,7 +32,9 @@ export default {
 </script>
 
 <style scoped>
-
+*:focus{
+  outline: none;
+}
 .main_txt{
   padding-top:100px;
   width:100%;
@@ -42,12 +45,37 @@ export default {
 }
 .searchBox{
   width:80%;
-  border:1px solid #575757;
+  /* border:1px solid #575757; */
   height:50px;
   border-radius:25px;
   margin:150px auto 30px auto;
 }
 .list{
   height:700px;
+}
+#search{
+  height:48px;
+  width:35%;
+  padding-left:50px;
+  border-radius: 25px 0 0 25px;
+  border:none;
+  float:left;
+}
+#onlineyn, #whom{
+  height:50px;
+  width:20%;
+  padding:0 20px;
+  float:left;
+  border:none;
+  margin-left:1px;
+}
+.searchBtn{
+  width:15%;
+  height:50px;
+  float:left;
+  margin-left:1px;
+  border:none;
+  border-radius:0 25px 25px 0;
+  background-color:#ffffff;
 }
 </style>
